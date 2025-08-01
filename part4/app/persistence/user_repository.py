b33397866490202266cba967d47f850f4f3ee835
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-08-01 17:48:17 +0200

    fixed

--- part4/app/persistence/user_repository.py
+++ part4/app/persistence/user_repository.py
@@ -1,22 +1,22 @@
 from app.models.user import User
 from app.persistence.repository import SQLAlchemyRepository
-
+from app.extensions import db
 
 class UserRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(User)
+    def __init__(self, session=db.session):
+        super().__init__(User, session)
 
     def get_user_by_email(self, email: str):
         """Return user instance matching the email."""
-        return self.session.query(User).filter_by(email=email).first()
+        return self.session.query(self.model).filter_by(email=email).first()
 
     def get_by_id(self, user_id: str):
         """Return user by ID."""
-        return self.session.query(User).filter_by(id=user_id).first()
+        return self.session.query(self.model).filter_by(id=user_id).first()
 
     def get_all(self):
         """Return all users."""
-        return self.session.query(User).all()
+        return self.session.query(self.model).all()
 
     def delete_by_id(self, user_id: str):
         """Delete user by ID."""
