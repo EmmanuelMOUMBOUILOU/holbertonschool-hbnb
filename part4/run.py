43709d9ca5781c9ef6d8ec243da6227bc48c6b9a
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-08-01 18:33:47 +0200

    Création des tables

--- part4/run.py
+++ part4/run.py
@@ -1,6 +1,15 @@
 from app import create_app
+from app.extensions import db
+from app.models import User, Place, Review, Amenity, place_amenity  # importe tous les modèles
 
 app = create_app()
 
+def setup_database():
+    with app.app_context():
+        # Création des tables si elles n'existent pas déjà
+        db.create_all()
+        print("✔️  Toutes les tables ont été créées.")
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000)
+    setup_database()
+    app.run(host='0.0.0.0', port=5000, debug=True)
