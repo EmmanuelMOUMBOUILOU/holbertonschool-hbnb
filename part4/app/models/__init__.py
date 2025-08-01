ee6099a58cf25573b819a0b6c38b07db19f32d95
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2025-08-01 18:33:08 +0200

    importé place_amenity

--- part4/app/models/__init__.py
+++ part4/app/models/__init__.py
@@ -1,5 +1,5 @@
 from .user import User
-from .place import Place
+from .place import Place, place_amenity
 from .review import Review
 from .amenity import Amenity
 from .base_model import BaseModel
